6ddc33136a98ae6c378edbc9c6709928d090267c
Devin Jenkins <dvnjenks@gmail.com>
2019-07-13 20:29:34 -0700

    added images folder w/ screenshoots..project not completely finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,19 +1,20 @@
-// what im pulling from 
-var inquirer = require("inquirer")
-var mysql = require ("mysql")
+// what im pulling from
+var inquirer = require("inquirer");
+var mysql = require("mysql");
 
 // creat the connection to mySQL
 var connect = mysql.createConnection({
-    host:"localhost",
-    PORT:3306,
-    user:"root",
-    password: "dj101489",
-    database:"bamazon"
-})
+  host: "localhost",
+  PORT: 3306,
+  user: "root",
+  password: "dj101489",
+  database: "bamazon"
+});
 
 // make sure connection works
-connect.connect(function(err){
-    if (err) throw err;
-    console.log("this is a good connection");
-    // connection works!!
-})
+connect.connect(function(err) {
+  if (err) throw err;
+  console.log("this is a good connection");
+  // connection works!!
+});
+// create a data rable in MYSQL.. link it to database.. then add prompts to js file.... ho over in class assingment for tips
